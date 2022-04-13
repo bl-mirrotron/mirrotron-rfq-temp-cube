5eaa106538ea8226ea9e98d13d29ede09036c2c5
David McGinnis <david.mcginnis@bl-mc.se>
2022-04-13 13:37:51 +0200

    updated on off for ds18b20

--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -2,8 +2,11 @@
 #include <OneWire.h>
 #define BAUD_RATE  19200
 #define commLEDPin    13
+#define waitLOW 500
+#define waitHIGH 500
+#define waitMEAS 1000
 
-#define BLINKYBUSBUFSIZE  11
+#define BLINKYBUSBUFSIZE  10
 union BlinkyBusUnion
 {
   struct
@@ -17,7 +20,6 @@ union BlinkyBusUnion
     int16_t i16_temp6;
     int16_t i16_temp7;
     int16_t i16_temp8;
-    int16_t i16_interval;
     int16_t i16_watchdog;
   };
   int16_t buffer[BLINKYBUSBUFSIZE];
@@ -43,106 +45,157 @@ DS18B20 dS18B20_6;
 DS18B20 dS18B20_7;
 DS18B20 dS18B20_8;
 
-unsigned long now;
-unsigned long lastMeasurement = 0;
-int ipoll = 0;
-
 void setup() 
 {
   dS18B20_1.signalPin = 6;
   dS18B20_1.powerPin = 5;
   pinMode(dS18B20_1.powerPin, OUTPUT);
-  digitalWrite(dS18B20_1.powerPin, HIGH);    
-  dS18B20_1.oneWire = OneWire(dS18B20_1.signalPin);
-  dS18B20_1.chipType = initDS18B20(dS18B20_1.address, &dS18B20_1.oneWire);
 
   dS18B20_2.signalPin = 8;
   dS18B20_2.powerPin = 7;
   pinMode(dS18B20_2.powerPin, OUTPUT);
   digitalWrite(dS18B20_2.powerPin, HIGH);    
-  dS18B20_2.oneWire = OneWire(dS18B20_2.signalPin);
-  dS18B20_2.chipType = initDS18B20(dS18B20_2.address, &dS18B20_2.oneWire);
 
   dS18B20_3.signalPin = 10;
   dS18B20_3.powerPin = 9;
   pinMode(dS18B20_3.powerPin, OUTPUT);
   digitalWrite(dS18B20_3.powerPin, HIGH);    
-  dS18B20_3.oneWire = OneWire(dS18B20_3.signalPin);
-  dS18B20_3.chipType = initDS18B20(dS18B20_3.address, &dS18B20_3.oneWire);
 
   dS18B20_4.signalPin = 12;
   dS18B20_4.powerPin = 11;
   pinMode(dS18B20_4.powerPin, OUTPUT);
-  digitalWrite(dS18B20_4.powerPin, HIGH);    
-  dS18B20_4.oneWire = OneWire(dS18B20_4.signalPin);
-  dS18B20_4.chipType = initDS18B20(dS18B20_4.address, &dS18B20_4.oneWire);
+  digitalWrite(dS18B20_4.powerPin, HIGH);   
 
   dS18B20_5.signalPin = 14;
   dS18B20_5.powerPin = 15;
   pinMode(dS18B20_5.powerPin, OUTPUT);
-  digitalWrite(dS18B20_5.powerPin, HIGH);    
-  dS18B20_5.oneWire = OneWire(dS18B20_5.signalPin);
-  dS18B20_5.chipType = initDS18B20(dS18B20_5.address, &dS18B20_5.oneWire);
+  digitalWrite(dS18B20_5.powerPin, HIGH);   
 
   dS18B20_6.signalPin = 16;
   dS18B20_6.powerPin = 17;
   pinMode(dS18B20_6.powerPin, OUTPUT);
-  digitalWrite(dS18B20_6.powerPin, HIGH);    
-  dS18B20_6.oneWire = OneWire(dS18B20_6.signalPin);
-  dS18B20_6.chipType = initDS18B20(dS18B20_6.address, &dS18B20_6.oneWire);
+  digitalWrite(dS18B20_6.powerPin, HIGH);   
 
   dS18B20_7.signalPin = 18;
   dS18B20_7.powerPin = 19;
   pinMode(dS18B20_7.powerPin, OUTPUT);
-  digitalWrite(dS18B20_7.powerPin, HIGH);    
-  dS18B20_7.oneWire = OneWire(dS18B20_7.signalPin);
-  dS18B20_7.chipType = initDS18B20(dS18B20_7.address, &dS18B20_7.oneWire);
+  digitalWrite(dS18B20_7.powerPin, HIGH);   
 
   dS18B20_8.signalPin = 20;
   dS18B20_8.powerPin = 21;
   pinMode(dS18B20_8.powerPin, OUTPUT);
+  digitalWrite(dS18B20_8.powerPin, HIGH);   
+
+  digitalWrite(dS18B20_1.powerPin, LOW);    
+  digitalWrite(dS18B20_2.powerPin, LOW);    
+  digitalWrite(dS18B20_3.powerPin, LOW);    
+  digitalWrite(dS18B20_4.powerPin, LOW);    
+  digitalWrite(dS18B20_5.powerPin, LOW);    
+  digitalWrite(dS18B20_6.powerPin, LOW);    
+  digitalWrite(dS18B20_7.powerPin, LOW);    
+  digitalWrite(dS18B20_8.powerPin, LOW);    
+  delay(1000);
+  digitalWrite(dS18B20_1.powerPin, HIGH);    
+  digitalWrite(dS18B20_2.powerPin, HIGH);    
+  digitalWrite(dS18B20_3.powerPin, HIGH);    
+  digitalWrite(dS18B20_4.powerPin, HIGH);    
+  digitalWrite(dS18B20_5.powerPin, HIGH);    
+  digitalWrite(dS18B20_6.powerPin, HIGH);    
+  digitalWrite(dS18B20_7.powerPin, HIGH);    
   digitalWrite(dS18B20_8.powerPin, HIGH);    
+  delay(2000);
+
+  dS18B20_1.oneWire = OneWire(dS18B20_1.signalPin);
+  dS18B20_1.chipType = initDS18B20(dS18B20_1.address, &dS18B20_1.oneWire);
+  dS18B20_2.oneWire = OneWire(dS18B20_2.signalPin);
+  dS18B20_2.chipType = initDS18B20(dS18B20_2.address, &dS18B20_2.oneWire);
+  dS18B20_3.oneWire = OneWire(dS18B20_3.signalPin);
+  dS18B20_3.chipType = initDS18B20(dS18B20_3.address, &dS18B20_3.oneWire);
+  dS18B20_4.oneWire = OneWire(dS18B20_4.signalPin);
+  dS18B20_4.chipType = initDS18B20(dS18B20_4.address, &dS18B20_4.oneWire);
+  dS18B20_5.oneWire = OneWire(dS18B20_5.signalPin);
+  dS18B20_5.chipType = initDS18B20(dS18B20_5.address, &dS18B20_5.oneWire);
+  dS18B20_6.oneWire = OneWire(dS18B20_6.signalPin);
+  dS18B20_6.chipType = initDS18B20(dS18B20_6.address, &dS18B20_6.oneWire);
+  dS18B20_7.oneWire = OneWire(dS18B20_7.signalPin);
+  dS18B20_7.chipType = initDS18B20(dS18B20_7.address, &dS18B20_7.oneWire);
   dS18B20_8.oneWire = OneWire(dS18B20_8.signalPin);
   dS18B20_8.chipType = initDS18B20(dS18B20_8.address, &dS18B20_8.oneWire);
 
+  
   Serial1.begin(BAUD_RATE);
   bb.state = 1; //init
   bb.i16_watchdog = 0; //init
-  bb.i16_interval = 10000;
   blinkyBus.start();
-  lastMeasurement = millis(); 
+  delay(5000);
   
-//  Serial.begin(9600);
+  Serial.begin(9600);
 
 }
 
 void loop() 
 {
-  now = millis();
-  if ((now - lastMeasurement) > bb.i16_interval)
-  {
-    lastMeasurement = now;
+    digitalWrite(dS18B20_1.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_1.powerPin, HIGH);
+    waitSome(waitHIGH);    
     dS18B20_1.temp = getDS18B20Temperature(&dS18B20_1.oneWire, dS18B20_1.address, dS18B20_1.chipType);
-    dS18B20_2.temp = getDS18B20Temperature(&dS18B20_2.oneWire, dS18B20_2.address, dS18B20_2.chipType);
-    dS18B20_3.temp = getDS18B20Temperature(&dS18B20_3.oneWire, dS18B20_3.address, dS18B20_3.chipType);
-    dS18B20_4.temp = getDS18B20Temperature(&dS18B20_4.oneWire, dS18B20_4.address, dS18B20_4.chipType);
-    dS18B20_5.temp = getDS18B20Temperature(&dS18B20_5.oneWire, dS18B20_5.address, dS18B20_5.chipType);
-    dS18B20_6.temp = getDS18B20Temperature(&dS18B20_6.oneWire, dS18B20_6.address, dS18B20_6.chipType);
-    dS18B20_7.temp = getDS18B20Temperature(&dS18B20_7.oneWire, dS18B20_7.address, dS18B20_7.chipType);
-    dS18B20_8.temp = getDS18B20Temperature(&dS18B20_8.oneWire, dS18B20_8.address, dS18B20_8.chipType);
-
     bb.i16_temp1  = (int16_t) (dS18B20_1.temp * 10.0);
+
+    digitalWrite(dS18B20_2.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_2.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_2.temp = getDS18B20Temperature(&dS18B20_2.oneWire, dS18B20_2.address, dS18B20_2.chipType);
     bb.i16_temp2  = (int16_t) (dS18B20_2.temp * 10.0);
+
+    digitalWrite(dS18B20_3.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_3.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_3.temp = getDS18B20Temperature(&dS18B20_3.oneWire, dS18B20_3.address, dS18B20_3.chipType);
     bb.i16_temp3  = (int16_t) (dS18B20_3.temp * 10.0);
+
+    digitalWrite(dS18B20_4.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_4.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_4.temp = getDS18B20Temperature(&dS18B20_4.oneWire, dS18B20_4.address, dS18B20_4.chipType);
     bb.i16_temp4  = (int16_t) (dS18B20_4.temp * 10.0);
+
+    digitalWrite(dS18B20_5.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_5.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_5.temp = getDS18B20Temperature(&dS18B20_5.oneWire, dS18B20_5.address, dS18B20_5.chipType);
     bb.i16_temp5  = (int16_t) (dS18B20_5.temp * 10.0);
+
+    digitalWrite(dS18B20_6.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_6.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_6.temp = getDS18B20Temperature(&dS18B20_6.oneWire, dS18B20_6.address, dS18B20_6.chipType);
     bb.i16_temp6  = (int16_t) (dS18B20_6.temp * 10.0);
+
+    digitalWrite(dS18B20_7.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_7.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_7.temp = getDS18B20Temperature(&dS18B20_7.oneWire, dS18B20_7.address, dS18B20_7.chipType);
     bb.i16_temp7  = (int16_t) (dS18B20_7.temp * 10.0);
+
+    digitalWrite(dS18B20_8.powerPin, LOW);
+    waitSome(waitLOW);    
+    digitalWrite(dS18B20_8.powerPin, HIGH);
+    waitSome(waitHIGH);    
+    dS18B20_8.temp = getDS18B20Temperature(&dS18B20_8.oneWire, dS18B20_8.address, dS18B20_8.chipType);
     bb.i16_temp8  = (int16_t) (dS18B20_8.temp * 10.0);
+
+    
     bb.i16_watchdog = bb.i16_watchdog + 1;
     if ( bb.i16_watchdog > 32760 ) bb.i16_watchdog = 0;
 
-/*
+
     Serial.print(bb.i16_temp1);
     Serial.print(" ");
     Serial.print(bb.i16_temp2);
@@ -162,16 +215,20 @@ void loop()
     Serial.print(bb.i16_watchdog);
     Serial.print(" ");
     Serial.println(" ");
-*/   
-  }
-  ipoll = blinkyBus.poll();
-  if (ipoll == 2)
-  {
-    if (blinkyBus.getLastWriteAddress() == 9) {} //measurement interval changed
-  }
-  blinkyBus.poll();
 }
-
+void waitSome(unsigned long milliWait)
+{
+  unsigned long now;
+  unsigned long startWait;
+  now = millis();
+  startWait = now;
+  while ((now -startWait) < milliWait)
+  {
+    now = millis();
+    blinkyBus.poll();
+  }
+  
+}
 byte initDS18B20(byte* addr, OneWire* ow)
 {
   byte type_s = 0;
@@ -208,8 +265,8 @@ float getDS18B20Temperature(OneWire* ow, byte* addr, byte chipType)
   ow->select(addr);
   ow->write(0x44, 1);        // start conversion, with parasite power on at the end
   
-  delay(750);     // maybe 750ms is enough, maybe not
-  
+//  delay(750);     // maybe 750ms is enough, maybe not
+  waitSome(waitMEAS);  
   ow->reset();
   ow->select(addr);    
   ow->write(0xBE);         // Read Scratchpad
